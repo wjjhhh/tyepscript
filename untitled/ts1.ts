92d19cd104ae87165bdee7c0e0b41767c050469b
wjj <378650926@qq.com>
2016-10-31 17:40:08 +0800

    10.31 2.2

--- untitled/ts1.ts
+++ untitled/ts1.ts
@@ -7,5 +7,16 @@ function createSquare(config:SquareConfig):{color:string;area:number}{
     if(config.color){
         newSquare.color=config.color;
     }
+    if(config.width){
+        newSquare.area=config.width*config.width;
+    }
+    return newSquare;
 
 }
+var mySquare=createSquare({color:'red',width:200});
+
+interface SearchFunc {
+    (source: string, subString: string): boolean;
+}
+var mySearch:SearchFunc;
+
