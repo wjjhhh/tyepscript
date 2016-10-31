8abe0642b785b1b4388128438f9f2084ac0d7562
wjj <378650926@qq.com>
2016-10-31 11:01:30 +0800

    10.31 2.0

--- untitled/ts1.ts
+++ untitled/ts1.ts
@@ -1,15 +1,13 @@
 /**
  * Created by Employee on 2016/10/28.
  */
-interface IUserinfo{
-    age:any;
-    name:string;
+class Greeter{
+    greeting:string;
+    constructor(message:string){
+        this.greeting=message;
+    }
+    greet(){
+        return "hello "+this.greeting;
+    }
 }
-function getInfo(user:IUserinfo):string{
-    return user.age+"====="+user.name;
-}
-var obj={name:12,age:100};
-(function(){
-    var test1=getInfo(obj);
-    console.log(test1);
-})()
+var greeter=new Greeter('world');
