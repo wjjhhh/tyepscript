22c05dfd3d233c48f0c3cc5ed8c9368ddfad2de3
wjj <378650926@qq.com>
2016-11-01 11:39:56 +0800

    3.1

--- untitled/js/ts1.js
+++ untitled/js/ts1.js
@@ -1,29 +1,38 @@
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
 var Person = (function () {
-    function Person(name, age) {
-        this.name = name;
-        this.age = age;
+    function Person() {
     }
+    Object.defineProperty(Person.prototype, "name", {
+        // constructor(name:string,age:number){
+        //     this.name=name;
+        //     this.age=age;
+        // }
+        get: function () {
+            return this._name;
+        },
+        set: function (newName) {
+            this._name = newName;
+        },
+        enumerable: true,
+        configurable: true
+    });
     Person.prototype.tell = function () {
-        return this.name + ";" + this.age;
+        return this._name + ";" + this.age;
     };
     return Person;
 }());
-var Student = (function (_super) {
-    __extends(Student, _super);
-    function Student(name, age, school) {
-        _super.call(this, name, age);
-        this.school = school;
-    }
-    Student.prototype.tell = function () {
-        return this.name + ":" + this.age + ":" + this.school;
-    };
-    return Student;
-}(Person));
-var st = new Student("jj", 12, "wefe");
-console.log(st.tell());
+// class Student extends Person{
+//     school:string;
+//     constructor(name:string,age:number,school:string){
+//         super(name,age)
+//         this.school=school;
+//     }
+//     tell(){
+//         return this.name+":"+this.age+":"+this.school;
+//     }
+// }
+// var st=new Student("jj",12,"wefe");
+// console.log(st.tell())
+var st2 = new Person();
+st2.name = '123';
+console.log(st2.tell());
 //# sourceMappingURL=ts1.js.map
--- untitled/ts1.ts
+++ untitled/ts1.ts
@@ -1,23 +1,33 @@
 class Person{
-    name:string;
+    private _name:string;
     age:number;
-    constructor(name:string,age:number){
-        this.name=name;
-        this.age=age;
+    // constructor(name:string,age:number){
+    //     this.name=name;
+    //     this.age=age;
+    // }
+    get name():string{
+        return this._name;
+    }
+    set name(newName:string){
+        this._name=newName;
     }
     tell(){
-        return this.name+";"+this.age;
+        return this._name+";"+this.age;
     }
 }
-class Student extends Person{
-    school:string;
-    constructor(name:string,age:number,school:string){
-        super(name,age)
-        this.school=school;
-    }
-    tell(){
-        return this.name+":"+this.age+":"+this.school;
-    }
-}
-var st=new Student("jj",12,"wefe");
-console.log(st.tell())
+// class Student extends Person{
+//     school:string;
+//     constructor(name:string,age:number,school:string){
+//         super(name,age)
+//         this.school=school;
+//     }
+//     tell(){
+//         return this.name+":"+this.age+":"+this.school;
+//     }
+// }
+// var st=new Student("jj",12,"wefe");
+// console.log(st.tell())
+
+var st2=new Person();
+st2.name='123';
+console.log(st2.tell());
